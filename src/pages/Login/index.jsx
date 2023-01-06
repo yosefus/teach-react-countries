3f1b6ce255f54fd2ce7972ab4493775e2c62e06f
Liran Aviani <liranaviani@Lirans-MacBook-Pro.local>
2023-01-06 10:24:37 +0200

    add login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,27 @@
-
+import { useContext, useState } from "react"
+import {LoginContext} from "../../layout/Layout/index"
 export default function Login() {
+   const { user, setUser } = useContext(LoginContext)
+   const {email, setEmail} = useState()
+   const {password, setPassword} = useState()
+
 
    return (
-      <div className='login-page'>
-         <form >
-            <h2>login</h2>
-            <input type="text" placeholder='email...' />
-            <input type="password" placeholder='password...' />
-            <button>Login</button>
-         </form>
-      </div>
-   )
+     <div className="login-page">
+       <form onSubmit={() => setUser({ email, password })}>
+         <h2>login</h2>
+         <input
+           onChange={(e) => setEmail(e.target.value)}
+           type="text"
+           placeholder="email..."
+         />
+         <input
+           onChange={(e) => setPassword(e.target.value)}
+           type="password"
+           placeholder="password..."
+         />
+         <button>Login</button>
+       </form>
+     </div>
+   );
 }
